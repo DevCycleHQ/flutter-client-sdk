205ec8767bac7401a682c8972ad604f3a09318e2
Laura Warr <laura.warr@taplytics.com>
2023-01-03 13:37:28 -0800

    Add event model

---
+++ lib/dvc_event.dart
@@ -0,0 +1,59 @@
+part of devcycle_flutter_client_sdk;
+
+class DVCEvent {
+  final String? type;
+  final String? target;
+  final double? value;
+  final Map<String, dynamic>? metaData;
+
+  DVCEvent._builder(DVCEventBuilder builder) :
+    type = builder._type,
+    target = builder._target,
+    value = builder._value,
+    metaData = builder._metaData;
+
+  Map<String, dynamic> _toMap() {
+    final Map<String, dynamic> result = <String, dynamic>{};
+    result['type'] = type;
+    result['target'] = target;
+    result['value'] = value;
+    result['metaData'] = metaData;
+    return result;
+  }
+}
+
+/// A builder for constructing [DVCEvent] objects.
+class DVCEventBuilder {
+  String? _type;
+  String? _target;
+  double? _value;
+  Map<String, dynamic>? _metaData;
+
+  /// Custom event type
+  DVCEventBuilder type(String type) {
+    this._type = type;
+    return this;
+  }
+
+  /// Custom event target / subject of event. Contextual to event type
+  DVCEventBuilder target(String target) {
+    this._target = target;
+    return this;
+  }
+
+  /// Sets the user's email attribute.
+  DVCEventBuilder value(double value) {
+    this._value = value;
+    return this;
+  }
+
+  DVCEventBuilder metaData(Map<String, dynamic> metaData) {
+    this._metaData = metaData;
+    return this;
+  }
+
+  /// Constructs a [DVCEvent] instance from the values currently in the builder.
+  DVCEvent build() {
+    return DVCEvent._builder(this);
+  }
+}
