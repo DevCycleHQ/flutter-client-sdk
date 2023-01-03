298a3ea1bb908657e785c279b31c87e500c3ae55
Laura Warr <laura.warr@taplytics.com>
2023-01-03 14:38:33 -0800

    Add tests for Feature class

---
+++ test/feature_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:devcycle_flutter_client_sdk/feature.dart';
+
+void main() {
+  test('builds feature object from a map', () {
+    Map<String, dynamic> featureMap = {
+      "id": "feature1",
+      "key": "feature-one",
+      "type": "release",
+      "variation": "variation1",
+      "variationName": "Variation One",
+      "variationKey": "var-one"
+    };
+    Feature feature = Feature.fromMap(featureMap);
+    expect(feature.id, equals('feature1'));
+    expect(feature.key, equals('feature-one'));
+    expect(feature.type.toString(), equals('FeatureType.release'));
+    expect(feature.variation, equals('variation1'));
+    expect(feature.variationName, equals('Variation One'));
+    expect(feature.variationKey, equals('var-one'));
+  });
+}
