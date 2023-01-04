f4d612259e0230a969217b2f3e3cd514fd04c0f0
Laura Warr <laura.warr@taplytics.com>
2023-01-04 13:21:28 -0800

    Add callback handler on client initialization

--- lib/devcycle_flutter_client_sdk.dart
+++ lib/devcycle_flutter_client_sdk.dart
@@ -1,23 +1,57 @@
+import 'package:flutter/services.dart';
 
 import 'devcycle_flutter_client_sdk_platform_interface.dart';
 import 'dvc_user.dart';
 import 'dvc_options.dart';
+import 'variable.dart';
 
 export 'dvc_user.dart';
 export 'dvc_event.dart';
 export 'dvc_options.dart';
 
+typedef ClientInitializedCallback = void Function(Error? error);
+typedef VariableUpdateCallback = void Function(Variable variable);
+typedef UserUpdateCallback = void Function(Map<String, Variable> variables);
 class DVCClient {
   String environmentKey;
   DVCUser user;
   DVCOptions? options;
+
+  static const _methodChannel = MethodChannel('devcycle_flutter_client_sdk');
+
+  /// Callback triggered on client initialization
+  ClientInitializedCallback? _clientInitializedCallback;
+  //. Map of variable keys to a list of variable update callbacks
+  Map<String, List<VariableUpdateCallback>> _variableUpdateCallbacks = Map();
+  // Map of callback IDs to user update callbacks. The ID is generated when identify is called
+  Map<String, UserUpdateCallback> _identifyCallbacks = Map();
+  // Map of callback IDs to user update callbacks. The ID is generated when reset is called
+  Map<String, UserUpdateCallback> _resetCallbacks = Map();
 
   DVCClient._builder(DVCClientBuilder builder) :
     environmentKey = builder._environmentKey!,
     user = builder._user!,
     options = builder._options;
 
+  Future<void> _handleCallbacks(MethodCall call) async {
+    switch (call.method) {
+      case 'clientInitialized':
+        if (_clientInitializedCallback != null) {
+          _clientInitializedCallback!(call.arguments);
+        }
+        break;
+      case 'variableUpdated':
+        Variable variable = Variable.fromCodec(call.arguments);
+        List<VariableUpdateCallback> callbacks = _variableUpdateCallbacks[variable.key] ?? [];
+        for (final callback in callbacks) {
+          callback(variable);
+        }
+        break;
+    }
+  }
+
   _init() {
+    _methodChannel.setMethodCallHandler(_handleCallbacks);
     DevCycleFlutterClientSdkPlatform.instance.initialize(environmentKey, user, options);
   }
 
