af285f7fbdc1117c02a884de62f963bfb45a514e
Laura Warr <laura.warr@taplytics.com>
2023-01-05 14:57:25 -0800

    Use mock method channel handler for testing

--- test/devcycle_flutter_client_sdk_test.dart
+++ test/devcycle_flutter_client_sdk_test.dart
@@ -1,47 +1,28 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:devcycle_flutter_client_sdk/devcycle_flutter_client_sdk.dart';
 import 'package:devcycle_flutter_client_sdk/devcycle_flutter_client_sdk_platform_interface.dart';
 import 'package:devcycle_flutter_client_sdk/devcycle_flutter_client_sdk_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-bool hasInitialized = false;
-bool hasIdentifiedUser = false;
-bool hasResetUser = false;
-
-class MockDevCycleFlutterClientSdkPlatform
-    with MockPlatformInterfaceMixin
-    implements DevCycleFlutterClientSdkPlatform {
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-
-  @override
-  void initialize(String environmentKey, DVCUser user, DVCOptions? options) {
-    hasInitialized = true;
-  }
-
-  @override
-  void identifyUser(DVCUser user, [String? callback]) {
-    hasIdentifiedUser = true;
-  }
-
-  @override
-  void resetUser([String? callback]) {
-    hasResetUser = true;
-  }
-}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
   final DevCycleFlutterClientSdkPlatform initialPlatform =
       DevCycleFlutterClientSdkPlatform.instance;
-  MockDevCycleFlutterClientSdkPlatform mockPlatform =
-      MockDevCycleFlutterClientSdkPlatform();
-  DevCycleFlutterClientSdkPlatform.instance = mockPlatform;
+  MethodChannelDevCycleFlutterClientSdk platform =
+      MethodChannelDevCycleFlutterClientSdk();
+
+  MethodCall? methodCall;
+  handler(MethodCall mc) async {
+    methodCall = mc;
+  }
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
+      .setMockMethodCallHandler(platform.methodChannel, handler);
+
+  DevCycleFlutterClientSdkPlatform.instance = platform;
 
   setUp(() {
-    hasInitialized = false;
-    hasIdentifiedUser = false;
-    hasResetUser = false;
+    methodCall = null;
   });
 
   test('$MethodChannelDevCycleFlutterClientSdk is the default instance', () {
@@ -51,13 +32,18 @@ void main() {
 
   group('initialize', () {
     test('initializes with only required params', () async {
-      DVCUser user = DVCUserBuilder().userId('123').build();
+      DVCUser user = DVCUserBuilder().userId('user1').build();
       DVCClientBuilder().user(user).environmentKey('123').build();
-      expect(hasInitialized, isTrue);
+      expect(methodCall?.method, 'initialize');
+      expect(methodCall?.arguments, {
+        "environmentKey": "123",
+        "user": {"userId": "user1"},
+        "options": null
+      });
     });
 
     test('initializes with all available params', () async {
-      DVCUser user = DVCUserBuilder().userId('123').build();
+      DVCUser user = DVCUserBuilder().userId('user1').build();
       DVCOptions options =
           DVCOptionsBuilder().configCacheTTL(100).enableEdgeDB(true).build();
       DVCClientBuilder()
@@ -65,7 +51,12 @@ void main() {
           .environmentKey('123')
           .options(options)
           .build();
-      expect(hasInitialized, isTrue);
+      expect(methodCall?.method, 'initialize');
+      expect(methodCall?.arguments, {
+        "environmentKey": "123",
+        "user": {"userId": "user1"},
+        "options": {"configCacheTTL": 100, "enableEdgeDB": true}
+      });
     });
 
     test('fails to initialize without an environment key', () async {
@@ -84,7 +75,6 @@ void main() {
 
   group('identifyUser', () {
     test('identify user correctly', () {
-      // TODO: update to mock the MethodChannel for proper testing
       DVCUser user1 = DVCUserBuilder()
           .userId('user1')
           .isAnonymous(false)
@@ -103,18 +93,22 @@ void main() {
       DVCClient devcycleFlutterClientSdkPlugin =
           DVCClientBuilder().environmentKey('SDK_KEY').user(user1).build();
       devcycleFlutterClientSdkPlugin.identifyUser(user2);
-      expect(hasIdentifiedUser, true);
-      // expect(devcycleFlutterClientSdkPlugin.user.userId, 'user2');
-      // expect(devcycleFlutterClientSdkPlugin.user.isAnonymous, isFalse);
-      // expect(devcycleFlutterClientSdkPlugin.user.email, 'test_2@example.com');
-      // expect(devcycleFlutterClientSdkPlugin.user.name, 'c d');
-      // expect(devcycleFlutterClientSdkPlugin.user.country, 'ca');
+      expect(methodCall?.method, 'identifyUser');
+      expect(methodCall?.arguments, {
+        'user': {
+          'userId': 'user2',
+          'isAnonymous': false,
+          'email': 'test_2@example.com',
+          'name': 'c d',
+          'country': 'ca'
+        },
+        'callbackId': null
+      });
     });
   });
 
   group('resetUser', () {
     test('reset user from client', () {
-      // TODO: update to mock the MethodChannel for proper testing
       DVCUser user = DVCUserBuilder()
           .userId('user1')
           .isAnonymous(false)
@@ -125,11 +119,8 @@ void main() {
       DVCClient devcycleFlutterClientSdkPlugin =
           DVCClientBuilder().environmentKey('SDK_KEY').user(user).build();
       devcycleFlutterClientSdkPlugin.resetUser();
-      expect(hasResetUser, true);
-      // expect(devcycleFlutterClientSdkPlugin.user.isAnonymous, isTrue);
-      // expect(devcycleFlutterClientSdkPlugin.user.email, isNot('test@example.com'));
-      // expect(devcycleFlutterClientSdkPlugin.user.name, isNull);
-      // expect(devcycleFlutterClientSdkPlugin.user.country, isNull);
+      expect(methodCall?.method, 'resetUser');
+      expect(methodCall?.arguments, {'callbackId': null});
     });
   });
 }
