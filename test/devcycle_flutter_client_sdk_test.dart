50cb869cb23bc9a3606e62cd65b814f2a0e9af30
Jonathan Norris <jonathan@taplytics.com>
2023-05-10 11:06:17 -0400

    update test

--- test/devcycle_flutter_client_sdk_test.dart
+++ test/devcycle_flutter_client_sdk_test.dart
@@ -154,7 +154,7 @@ void main() {
       DVCClientBuilder().sdkKey('SDK_KEY').user(user).build();
       await devcycleFlutterClientSdkPlugin.variableValue(
           'test-key', 'default-value');
-      expect(methodCall?.method, 'variableValue');
+      expect(methodCall?.method, 'variable');
       expect(methodCall?.arguments,
           {'key': 'test-key', 'defaultValue': 'default-value'});
     });
